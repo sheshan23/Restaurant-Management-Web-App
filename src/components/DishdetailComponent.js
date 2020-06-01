837fb09585e63c0a62d47441ba65ae77ed740eea
sheshan23 <patelsheshan0@gmail.com>
2020-06-01 12:27:06 -0400

    React animation componemts

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Errors, Control } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -12,13 +13,19 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
     function RenderDish({dish}) {
         return (
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     }
 
@@ -27,17 +34,19 @@ const minLength = (len) => (val) => val && (val.length >= len);
             return(
                 <div key={c.id}>
                     <ul class="list-unstyled">
-                        <li>
-                            <p>{c.comment}</p>
-                            <p>-- {c.author},
-                            &nbsp; 
-                            {new Intl.DateTimeFormat('en-US', 
-                                { year: 'numeric', 
-                                    month: 'short', 
-                                    day: '2-digit'
-                                }).format(new Date(Date.parse(c.date)))}
-                            </p>
-                        </li>
+                        <Fade in>
+                            <li>
+                                <p>{c.comment}</p>
+                                <p>-- {c.author},
+                                &nbsp; 
+                                {new Intl.DateTimeFormat('en-US', 
+                                    { year: 'numeric', 
+                                        month: 'short', 
+                                        day: '2-digit'
+                                    }).format(new Date(Date.parse(c.date)))}
+                                </p>
+                            </li>
+                        </Fade>
                     </ul>
                 </div>
             );
@@ -46,7 +55,9 @@ const minLength = (len) => (val) => val && (val.length >= len);
             return(
                 <div>
                     <h4>Comments</h4>
-                    {comment}
+                    <Stagger in>
+                        {comment}
+                    </Stagger>
                     <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
