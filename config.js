e5894ac23454bd1e115371c46a9ee5836a39869e
sheshan23 <patelsheshan0@gmail.com>
2020-07-30 17:01:47 -0400

    Integrated to mongodb atlas

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'mongoUrl': 'mongodb+srv://admin:admin@cluster0.dyfev.mongodb.net/conFusion?retryWrites=true&w=majority', 
     'facebook': {
         clientId: '313045163189922',
         clientSecret: 'c7d7bf9f2fbf2aadf3f04cda5fa4eba0'
     }
 }
+
+// mongodb://localhost:27017/conFusion
